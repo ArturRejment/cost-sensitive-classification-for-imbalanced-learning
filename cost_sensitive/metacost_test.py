af16dcee4b959c7092e0c92f98cc548ebd83bb3a
Artur Rejment <arturrejment1@gmail.com>
2024-01-21 22:58:37 +0100

    conduct tests, create plots and tables

--- cost_sensitive/metacost_test.py
+++ cost_sensitive/metacost_test.py
@@ -23,26 +23,26 @@ cost_matrixes = [
     # np.array([[0, 1], [20, 0]]),
 ]
 
-datasets_init = [
-    "../datasets/cleveland-0_vs_4.csv",
-    "../datasets/dermatology-6.csv",
-    "../datasets/ecoli3.csv",
-    "../datasets/ecoli-0-6-7_vs_3-5.csv",
-    "../datasets/haberman.csv",
-    "../datasets/pima.csv",
-    "../datasets/wisconsin.csv",
-    "../datasets/flare-F.csv",
-    "../datasets/shuttle-c0-vs-c4.csv",
-    "../datasets/yeast3.csv",
-    "../datasets/stroke.csv",
-]
+datasets_init = {
+    "../datasets/cleveland-0_vs_4.csv": np.array([[0, 1], [20, 0]]),
+    "../datasets/dermatology-6.csv": np.array([[0, 1], [20, 0]]),
+    "../datasets/ecoli3.csv": np.array([[0, 1], [5, 0]]),
+    "../datasets/ecoli-0-6-7_vs_3-5.csv": np.array([[0, 1], [10, 0]]),
+    "../datasets/haberman.csv": np.array([[0, 1], [5, 0]]),
+    "../datasets/pima.csv": np.array([[0, 1], [5, 0]]),
+    "../datasets/wisconsin.csv": np.array([[0, 1], [10, 0]]),
+    "../datasets/flare-F.csv": np.array([[0, 1], [10, 0]]),
+    "../datasets/shuttle-c0-vs-c4.csv": np.array([[0, 1], [10, 0]]),
+    "../datasets/yeast3.csv": np.array([[0, 1], [20, 0]]),
+    "../datasets/stroke.csv": np.array([[0, 1], [20, 0]]),
+}
 
 # PREPARE DATASETS
 datasets = []
 
 le = LabelEncoder()
 
-for dataset in datasets_init:
+for dataset in datasets_init.keys():
     df = pd.read_csv(dataset)
     for header in df.columns:
         label = le.fit_transform(df[header])
@@ -59,7 +59,7 @@ clfs = [
 
 rskf = RepeatedStratifiedKFold(n_splits=3, n_repeats=2, random_state=1410)
 
-scores = np.zeros(shape=(len(datasets), len(clfs) * len(cost_matrixes), rskf.get_n_splits()))
+scores = np.zeros(shape=(len(datasets), len(clfs), rskf.get_n_splits()))
 
 for dataset_idx, dataset in enumerate(datasets):
     data = dataset.values
@@ -68,24 +68,23 @@ for dataset_idx, dataset in enumerate(datasets):
 
     for clf_idx, est in enumerate(clfs):
 
-        for cost_matrix_idx, cost_matrix in enumerate(cost_matrixes):
+        bac = []
+        for fold_idx, (train, test) in enumerate(rskf.split(X, y)):
+            X_train = X[train]
+            X_test = X[test]
+            y_train = y[train]
 
-            bac = []
-            for fold_idx, (train, test) in enumerate(rskf.split(X, y)):
-                X_train = X[train]
-                X_test = X[test]
-                y_train = y[train]
+            est_cloned = clone(est)
+            clf = MetaCost(est_cloned, cost_matrixes[0])
+            # clf = MetaCost(est_cloned, list(datasets_init.values())[dataset_idx])
+            clf = clf.fit_transform(X_train, y_train)
+            y_pred = clf.predict(X_test)
 
-                est_cloned = clone(est)
-                clf = MetaCost(est_cloned, cost_matrix)
-                clf = clf.fit_transform(X_train, y_train)
-                y_pred = clf.predict(X_test)
+            balanced_accuracy = balanced_accuracy_score(y[test], y_pred)
+            scores[dataset_idx, clf_idx, fold_idx] = balanced_accuracy
+            bac.append(balanced_accuracy)
 
-                balanced_accuracy = balanced_accuracy_score(y[test], y_pred)
-                scores[dataset_idx, clf_idx, fold_idx] = balanced_accuracy
-                bac.append(balanced_accuracy)
-
-            print("META COST ", np.mean(bac))
-            print('\n')
+        print(dataset_idx, clf_idx, "META COST ", np.mean(bac))
+        print('\n')
 
 np.save("meta_cost_scores", scores)
--- cost_sensitive/plots.py
+++ cost_sensitive/plots.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from prettytable import PrettyTable
+from tabulate import tabulate
 
 weights_names = [
     "None",
@@ -28,21 +30,77 @@ datasets_names = [
 
 scores = np.load("scores.npy")
 
+t = PrettyTable([
+    "Classifier",
+    
+    "DT None",
+    "DT 5:1",
+    "DT 10:1",
+    "DT 20:1",
+    "DT 1:5",
+    "DT 1:10",
+    "DT 1:20",
+    "DT balanced",
+
+])
+
+x = PrettyTable([
+    "Classifier",
+
+    "RF None",
+    "RF 5:1",
+    "RF 10:1",
+    "RF 20:1",
+    "RF 1:5",
+    "RF 1:10",
+    "RF 1:20",
+    "RF balanced",
+])
+
+z = PrettyTable([
+    "Classifier",
+
+    "SVC None",
+    "SVC 5:1",
+    "SVC 10:1",
+    "SVC 20:1",
+    "SVC 1:5",
+    "SVC 1:10",
+    "SVC 1:20",
+    "SVC balanced",
+])
+
 for idx, dataset_name in enumerate(datasets_names):
     b = np.mean(scores[idx], axis=1)
-    c = np.mean(b, axis=1)
+    c = np.mean(b, axis=1, dtype=float)
+    #
+    # row = [dataset_name]
+    # for i in c[:8]:
+    #     row.append(round(i, 3))
+    # t.add_row(row)
+    #
+    #
+    # row = [dataset_name]
+    # for i in c[8:16]:
+    #     row.append(round(i, 3))
+    # x.add_row(row)
+    #
+    # row = [dataset_name]
+    # for i in c[16:]:
+    #     row.append(round(i, 3))
+    # z.add_row(row)
+    #
 
-    plt.plot(weights_names, c[:8])
-    plt.title(dataset_name + " Decision Tree")
-    plt.show()
+    plt.plot(weights_names, c[:8], label="Decision Tree")
 
-    plt.plot(weights_names, c[8:16])
-    plt.title(dataset_name + " Random Forrest")
-    plt.show()
+    plt.plot(weights_names, c[8:16], label="Random Forrest")
 
-    plt.plot(weights_names, c[16:])
-    plt.title(dataset_name + " SVC")
-    plt.show()
+    plt.plot(weights_names, c[16:], label="SVC")
+    plt.title(dataset_name)
+    plt.legend()
+    plt.savefig(f"../figs/{dataset_name}.jpg")
+    plt.clf()
 
-    exit()
-
+print(t.get_latex_string())
+print(x.get_latex_string())
+print(z.get_latex_string())
